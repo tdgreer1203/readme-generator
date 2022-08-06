97345d213a176b1505f889935e1a966c36893943
Theo <tdgreer1203@gmail.com>
2022-08-06 07:47:44 -0400

    Completed Questions Array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,89 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const createReadme = require('./utils/createReadme');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title (required): ',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation: '
+    },
+    {
+        type: 'input',
+        name: 'reason',
+        message: 'Why did you build the project: '
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve: '
+    },
+    {
+        type: 'input',
+        name: 'learned',
+        message: 'What did you learn from creating it: ',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How can the app be installed: '
+    },
+    {
+        type: 'input',
+        name: 'useage',
+        message: 'How do we use the app: '
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Enter any credits: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the type of license: ',
+        choices: ['MIT', 'Unlicense', 'GPLv2', 'Apache', 'GPLv3', 'What the Fuck you Want']
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List the project features here: '
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can others contribute: '
+    },
+    {
+        type: 'confirm',
+        name: 'testsConfirm',
+        message: 'Do you have any tests for this project: ',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter any tests for the project: ',
+        when: ({testsConfirm}) => {
+            return(testsConfirm ? true : false)
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +93,23 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+/*
+README Outline
+    Title
+    Description
+        What was your motivation?
+        Why did you build this project?
+        What problem does it solve?
+        What did you learn?
+        What makes your project stand out?
+    Table of Contents
+        Installation
+        Usage
+        Credits
+        License
+    Features
+    How to Contribute
+    Tests
+*/
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,3 +18,12 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+/* 
+Popular Licenses
+1. [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+2. [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+3. [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+4. [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+5. [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+6. [![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+*/
